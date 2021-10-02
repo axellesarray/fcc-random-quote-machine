d4746ade89f827c57852179ca87276775cd7b1b9
axellesarray <axelle.sarray@gmail.com>
2021-10-02 14:34:06 +0200

    completed react project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import COLORS_ARRAY from "./colorsArray";
+import QUOTES_ARRAY from "./quotesArray";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [quote, setQuote] = useState(
+    "In order to succeed, your desire for success should be greater than your fear of failure."
+  );
+  const [author, setAuthor] = useState("Bill Cosby");
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [accentColor, setAccentColor] = useState("#809980");
+
+  const getRandomQuote = () => {
+    let randomInteger = Math.floor(QUOTES_ARRAY.length * Math.random());
+    setRandomNumber(randomInteger);
+    setAccentColor(COLORS_ARRAY[randomInteger]);
+    setQuote(QUOTES_ARRAY[randomInteger].quote);
+    setAuthor(QUOTES_ARRAY[randomInteger].author);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ backgroundColor: accentColor }}>
+        <div id="quote-box">
+          <FontAwesomeIcon
+            id="quote-icon"
+            icon={faQuoteLeft}
+            style={{ color: accentColor }}
+          />
+
+          <p id="text" style={{ color: accentColor }}>
+            {quote}
+          </p>
+          <p id="author" style={{ color: accentColor }}>
+            - {author}
+          </p>
+
+          {/* Placed those 2 next elements in order to pass the FreeCodeCamp test (had to be within the #quote-box element), but I wanted my app's design to be different => {display: none;} in "./App.scss" */}
+          <button id="new-quote">Quote</button>
+          <a
+            href={encodeURI(
+              `http://www.twitter.com/intent/tweet?text="${quote}" - ${author}`
+            )}
+            id="tweet-quote"
+          >
+            Twitter
+          </a>
+          {/* End of the {display: none;} */}
+        </div>
+
+        <div class="links">
+          <button
+            class="new-quote"
+            onClick={() => getRandomQuote()}
+            timeOut={350}
+            style={{ backgroundColor: accentColor }}
+          >
+            New Quote
+          </button>
+          <a
+            href={encodeURI(
+              `http://www.twitter.com/intent/tweet?text="${quote}" - ${author}`
+            )}
+            target="_blank"
+            rel="noreferrer"
+            class="tweet-quote"
+            style={{ backgroundColor: accentColor }}
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
       </header>
     </div>
   );
---
+++ src/quotesArray.js
@@ -0,0 +1,113 @@
+const QUOTES_ARRAY = [
+  {
+    quote: "Be yourself, everyone else is already taken.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    author: "Bernard M. Baruch",
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  },
+  {
+    quote:
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    quote:
+      "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote:
+      "Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.",
+    author: "Marilyn Monroe",
+  },
+  {
+    quote:
+      "Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.",
+    author: "Neil Gaiman",
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas A. Edison",
+  },
+  {
+    quote:
+      "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+    author: "Douglas Adams",
+  },
+  {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot",
+  },
+  {
+    quote:
+      "If you can't explain it to a six year old, you don't understand it yourself.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "We don't see things as they are, we see them as we are.",
+    author: "Anaïs Nin",
+  },
+  {
+    quote: "If you tell the truth, you don't have to remember anything.",
+    author: "Mark Twain",
+  },
+  {
+    quote:
+      "Sometimes the questions are complicated and the answers are simple.",
+    author: "Dr. Seuss",
+  },
+  {
+    quote:
+      "Logic will get you from A to Z; imagination will get you everywhere.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "One good thing about music, when it hits you, you feel no pain.",
+    author: "Bob Marley",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston S. Churchill",
+  },
+  {
+    quote: "Do one thing every day that scares you.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "Be the change that you wish to see in the world",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote:
+      "Our greatest glory is not in never falling, but in rising every time we fall.",
+    author: "Oliver Goldsmith",
+  },
+  {
+    quote:
+      "It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things.",
+    author: "Leonardo da Vinci",
+  },
+  {
+    quote: "Whatever the mind can conceive and believe, it can achieve.",
+    author: "Napoleon Hill",
+  },
+];
+
+export default QUOTES_ARRAY;
